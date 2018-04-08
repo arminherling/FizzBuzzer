071d6e082faea072aacdd0ab0e6aef39d4472e62
Armin Herling <armin.herling@gmail.com>
2018-04-08 23:02:57 +0200

    Added projects

---
+++ FizzBuzzer.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NUnit.Framework;
+
+namespace FizzBuzzer.Tests
+{
+    [TestFixture]
+    public class UnitTest1
+    {
+        [Test]
+        public void TestMethod1()
+        {
+        }
+    }
+}
